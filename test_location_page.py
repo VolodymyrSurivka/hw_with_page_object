f0cc4b4a56642cd1a8032fef8f8ef844900576ef
Volodymyr <volodymyrsurivka@gmail.com>
2020-10-01 00:05:34 +0300

    Added test_location_page.py

---
+++ test_location_page.py
@@ -0,0 +1,18 @@
+from .pages.login_page import LoginPage
+from .pages.location_page import LocationPage
+import pytest
+
+
+@pytest.mark.need_test
+class TestAddNewLocation:
+    def test_user_can_login_in_bo(self, browser):
+        bo_login_link = "https://back-office.dev.it-banda.com/login"
+        page = LoginPage(browser, bo_login_link)
+        page.open()
+        page.login_in_bo(login="vladimir.surivka", password="1q2w3e4r")
+
+    def test_user_can_add_new_location(self, browser):
+        retail_software_locations_link = "https://back-office.dev.it-banda.com/retail-software/locations"
+        page = LocationPage(browser, retail_software_locations_link)
+        page.open()
+        page.add_new_location()
