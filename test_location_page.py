e40b36d67b387586d1de7799018387e975a38c78
Volodymyr <volodymyrsurivka@gmail.com>
2020-10-01 00:19:42 +0300

    Edited test_location_page.py

--- test_location_page.py
+++ test_location_page.py
@@ -5,13 +5,12 @@ import pytest
 
 @pytest.mark.need_test
 class TestAddNewLocation:
-    def test_user_can_login_in_bo(self, browser):
+
+    def test_user_can_add_new_location(self, browser):
         bo_login_link = "https://back-office.dev.it-banda.com/login"
         page = LoginPage(browser, bo_login_link)
         page.open()
         page.login_in_bo(login="vladimir.surivka", password="1q2w3e4r")
-
-    def test_user_can_add_new_location(self, browser):
         retail_software_locations_link = "https://back-office.dev.it-banda.com/retail-software/locations"
         page = LocationPage(browser, retail_software_locations_link)
         page.open()
